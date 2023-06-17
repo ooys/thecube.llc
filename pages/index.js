ba02bf394b803b38c12d1f96b5427acec008398e
Yihong Song <ys377@duke.edu>
2023-06-16 17:16:51 -0700

    init website

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <>
+            <div className="pranaynay-box">
+                <img src="/images/pranaynay.jpg" alt="Pranaynay" className="pranaynay" />
+            </div>
+        </>
+    );
+}
